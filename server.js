7e5c8df85af61288e655ecacefa06d038fb65b14
Ryan Huse <ryan.huse@alaskaair.com>
2020-02-22 15:48:55 -0800

    Added pulling meta data

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
+const {
+  BlobServiceClient,
+  StorageSharedKeyCredential
+} = require("@azure/storage-blob");
 require("dotenv").config();
 
 const app = express();
@@ -9,9 +13,65 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// [Node.js only] A helper method used to read a Node.js readable stream into string
+async function streamToString(readableStream) {
+  return new Promise((resolve, reject) => {
+    const chunks = [];
+    readableStream.on("data", data => {
+      chunks.push(data.toString());
+    });
+    readableStream.on("end", () => {
+      resolve(chunks.join(""));
+    });
+    readableStream.on("error", reject);
+  });
+}
+
 // API calls
-app.get("/api/hello", (req, res) => {
-  res.send({ express: "Hello From Express" });
+app.get("/api/hello", async (req, res) => {
+  const account = "iatahackathonstorage";
+  const accountKey =
+    "tDmDvkKhrTDPIf3WuM40GfVuDefqc3YC41mcU39UkHn2GZrDZ09bLRV4fIpDLTylK5dWeNiESKY58DfmZU2bAg==";
+  const sharedKeyCredential = new StorageSharedKeyCredential(
+    account,
+    accountKey
+  );
+
+  const blobServiceClient = new BlobServiceClient(
+    `https://${account}.blob.core.windows.net`,
+    sharedKeyCredential
+  );
+
+  const containerName = "requests";
+
+  const containerClient = blobServiceClient.getContainerClient(containerName);
+  i = 1;
+  var imageNames = [];
+  for await (const blob of containerClient.listBlobsFlat()) {
+    if (blob.name.startsWith(req.query.barcodeID + "/")) {
+      imageNames.push(blob.name);
+      console.log(`Blob ${i++}: ${blob.name}`);
+    }
+  }
+  var promises = imageNames.map(async (element, index) => {
+    let blobClient = containerClient.getBlobClient(element);
+    // Get blob content from position 0 to the end
+    // In Node.js, get downloaded data by accessing downloadBlockBlobResponse.readableStreamBody
+    let downloadBlockBlobResponse = await blobClient.download();
+    let metadata = downloadBlockBlobResponse.metadata;
+    let downloaded = await streamToString(
+      downloadBlockBlobResponse.readableStreamBody
+    );
+    return {
+      data: downloaded,
+      metadata: {
+        airline: metadata.airline,
+        airport: metadata.airport
+      }
+    };
+  });
+  const images = await Promise.all(promises);
+  res.send(images);
 });
 
 app.post("/api/world", (req, res) => {
